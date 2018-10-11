49532f39ef3bee495a8d8494f3041e588e083895
YukiTakayama <44046719+YukiTakayama@users.noreply.github.com>
2018-10-11 14:45:48 +0900

    Update Paiza/paiza-1-08-1.py

--- Paiza/paiza-1-08-1.py
+++ Paiza/paiza-1-08-1.py
@@ -14,7 +14,8 @@
 スライムの合計体重はYYキロです
 '''
 import random
-number = random.randint(1, 10)  # 匹数 1 ～ 10
+number = random.randint(1, 10)  # 匹数 1 ～10
+
 
 print("体重100キロのスライムが" + str(number) + "匹あらわれた")
 # 合計体重 = 匹数 x 100
